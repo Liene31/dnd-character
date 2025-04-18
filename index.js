43d186a437cc3d51c657306d34cceb408377091a
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-04-18 15:22:16 +0200

    Finalised the Modal

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
 const modalForm = document.getElementById("modal-form");
-const acceptBtn = document.getElementById("accept-btn");
+// const acceptBtn = document.getElementById("accept-btn");
 const modalParagraph = document.getElementById("modal-p");
+const modalInner = document.getElementById("modal-inner");
 
 // Currently with cusrsor NOT ALLOWED still can close the window
 // Need to change the cursor to pointer when allowed to close
@@ -18,6 +19,9 @@ modalCloseBtn.addEventListener("click", function () {
 modalForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
+  const userFormData = new FormData(modalForm);
+  const userName = userFormData.get("fullName");
+
   modalParagraph.innerHTML = `
   <div class="modal-inner-loading">
     <img class="modal-loading-img" src="images/dnd_loading.gif" />
@@ -31,6 +35,13 @@ modalForm.addEventListener("submit", function (event) {
   }, 1800);
 
   setTimeout(function () {
-    modal.textContent = `TEST`;
+    modalInner.innerHTML = `
+      <h2 class="modal-header">Thanks <span class="bold color-white">${userName}</span>, you sucker!</h2>
+      <p class="modal-p">
+        May the gods bless your ignorance. Your soul has been sold.<br> </br>
+        Think of it as a transfer of ownership — you still get to use your soul… but we decide how.
+      </p>
+      <img class="devil-laughing-gif" src="images/devil-laughing.gif" />
+          `;
   }, 4000);
 });
